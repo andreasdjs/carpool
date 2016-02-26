fc8052ca56aa774fdbc6c61b03e7ca05d2456dc7
andreasdjs <andreas.dagnell@gmail.com>
2016-02-26 12:49:15 +0100

    Added Mocha, Chai, Sinon test files and dev dependencies.

---
+++ modules/modules.js
@@ -0,0 +1,4 @@
+exports.myFunction = function(parameter) {
+	var output = parameter;
+	return output;
+}
---
+++ test/test.js
@@ -0,0 +1,44 @@
+var expect = require('chai').expect;
+var myFunction = require('../modules/modules').myFunction;
+// var getInfoLang = require('./async').getInfoLang;
+var sinon = require('sinon');
+var chai = require('chai');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+describe('Get info from .JSON-file on GitHub.', function() {
+	it('Empty test.', function(done) {
+		console.log("output: " + myFunction(0));
+		expect(myFunction(0)).is.a('number');
+		done();
+	});
+
+/*
+	it('returns information from GitHub', function(done) {
+		getInfo(function(reply) {
+			expect(reply.language).to.equal('JavaScript');
+			expect(reply.owner.login).to.equal('xedric');
+			done();
+		});
+	});*/
+});
+
+describe('Test of callback function.', function() {
+
+// Test callback function
+
+/*
+	it('returns language', function(done) {
+		var ghLang = {
+			'language': 'GUGGE'
+		};
+		var stub = sinon.stub().callsArgWith(0, ghLang);
+
+		getInfoLang(stub, function(reply) {
+			expect(reply).to.equal('Language is GUGGE');
+			done();
+		});
+	});
+*/
+
+});
