390b5a1e8988a4c21e8718ff96bb99aad8fa6dcc
andreasdjs <andreas.dagnell@gmail.com>
2016-02-29 22:43:22 +0100

    Added users listning.

--- modules/carpool.js
+++ modules/carpool.js
@@ -11,9 +11,24 @@ function readVehicles(callback) {
   fileReadStream.on('end', () => {
       var obj = JSON.parse(data);
       console.log(data);
-//      console.log(obj);      
+      callback(obj); // Send object back
+  });
+}
+
+function readUsers(callback) {
+  var fileReadStream = fs.createReadStream('usersModel.json');
+  var data = "";
+
+  fileReadStream.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  fileReadStream.on('end', () => {
+      var obj = JSON.parse(data);
+      console.log(data);
       callback(obj); // Send object back
   });
 }
 
 module.exports.readVehicles = readVehicles;
+module.exports.readUsers = readUsers;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var fs = require('fs');
+var carpool = require('../modules/carpool');
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+/* 
+	Getting the data object from readCoffee function and 
+  	passing the object to the JADE template.
+*/
+
+	carpool.readUsers(pushContent);
+
+
+    function pushContent(obj){
+
+    	console.log('Object from readUsers:\n\n' + obj);
+
+    	res.render('users', {
+        	title: 'Users',
+        	users: obj
+    	});
+
+  	}
 });
 
 module.exports = router;
