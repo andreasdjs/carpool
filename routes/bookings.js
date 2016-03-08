4573754f1f7e54a36e121d20e1d14dcee8629621
andreasdjs <andreas.dagnell@gmail.com>
2016-03-08 18:43:47 +0100

    Adding filtering bookings by user on page showing bookings.

--- routes/bookings.js
+++ routes/bookings.js
@@ -7,8 +7,8 @@ var carpool = require('../modules/carpool');
 router.get('/', function(req, res, next) {
 
 /* 
-	Getting the data object from readCoffee function and 
-  	passing the object to the JADE template.
+	Getting the data object from readBookings function and 
+  passing the object to the JADE template.
 */
 
 	carpool.readBookings(pushContent);
@@ -17,7 +17,21 @@ router.get('/', function(req, res, next) {
 
     function pushContent(obj){
 
-    	// console.log('object from readBookings:\n\n' + JSON.stringify(obj));
+      console.log('object from readBookings:\n\n' + JSON.stringify(obj));
+
+      /* Filter based on logged in user. 
+         Show only the users bookings if normal user. 
+         Admin can see all. */
+
+      if("admin" !== req.cookies.username){
+
+        obj.bookings = obj.bookings.filter(function (el) {
+          return el.userId == req.cookies.employeeNumber;
+        });
+
+        console.log('New object from readBookings:\n\n' + JSON.stringify(obj));
+
+      }
 
     	res.render('bookings', {
           username: req.cookies.username,
